6737308c86f1e339b8a60378da3862fca2f16457
Oleg <o.sas@aisnovations.com>
2017-01-11 17:55:10 +0300

    The next step - is to re-import the rates table without spaces between the numbers

--- cashflowPlus.js
+++ cashflowPlus.js
@@ -27,8 +27,8 @@ function dataRates(){
         
         timeDay = Math.floor(standartDateA[i].getTime()/(1000*60*60*24));// we find a day since zero point
         rateInDaysA[timeDay] = rateA[i];
-        print("timeDay = " + timeDay);
-        print("rateInDaysA[timeDay] = "+ rateInDaysA[timeDay])
+        // print("timeDay = " + timeDay);
+        // print("rateInDaysA[timeDay] = "+ rateInDaysA[timeDay])
     }
     ratesH.data = dataA;
     ratesH.rateInDays = rateInDaysA;
@@ -162,9 +162,10 @@ function runCashFlow(begin, end){// we want to use day from the begining Day 197
     }
 }
 
-db.cashflow.remove({});
 
 function exchange(nowTimeDay, ratesH, amount, fromCurrency, toCurrency){
+    var fromCurency;
+    var toCurrency;
     // nowTimeDay in days from zero point
     //fromCurrency = "Byr", "Byn", "Usd"
     //toCurrency = "Byr", "Byn", "Usd"
@@ -183,21 +184,60 @@ function exchange(nowTimeDay, ratesH, amount, fromCurrency, toCurrency){
     }
     if((toCurrency === "Byr") || (toCurrency === "Byn")){
         if(toCurrency === "Byr"){
-            fromUsd = amount; toByr = amount*rate;
+            fromUsd = amount; /*toByr = amount*rate;*/
         } else{
             fromUsd = amount; toByn = amount*rate;
         }
     }
     exchangeResultA[0] = fromByr; exchangeResultA[1] = fromByn; exchangeResultA[2] = fromUsd;
     exchangeResultA[3] = toByr; exchangeResultA[4] = toByn; exchangeResultA[5] = toUsd;
-
+    print("fromByr = " + fromByr);
+    print("fromUsd = "+ fromUsd);
+    print("toByr = "+ toByr); 
+    print("toUsd = " + toUsd);
+    print("nowTimeDay = " + nowTimeDay);
+    print("amount = "+ amount);
+    print("rate = " + rate);
+    print(typeof rate);
     return exchangeResultA;
 }
 
-function runCashFlowPLus(begin, end){// we want to use day from the begining Day 1970
+function ifWeNeedExchange(cycleTimeDay, Byr, Byn, Usd){
+    if (Byr < 0){
+        print("Byr is = " + Byr);
+        exchange(cycleTimeDay, ratesH, Usd, "Usd", "Byr");
+    }
+}
 
+function runCashFlowPLus(begin, end){// we want to use day from the begining Day 1970
+    //ratesH.data is in a string format
+    var startDATE = standartDate(begin);
+    var startTimeDay = Math.floor(startDATE.getTime()/(1000*60*60*24));
+    var finishDATE = standartDate(end);
+    /////////////////////var finishTimeDay = Math.floor(finishDATE.getTime()/(1000*60*60*24));// for the debug only
+    var finishTimeDay = 14630;// debug only////////////////////////////////////
+    //startTimeDay = 14610
+    //finishTimeDay = 17130
+    // number of the days is finishTimeDay-startTimeDay+1 = 2521
+    var flowcashboxA = []; // flowcashboxA is the global cashbox, it is the cashflow
+    flowcashboxA[0] = 0; flowcashboxA[1] = 0; flowcashboxA[2] = 0;
+    // let cashboxA[0] = Byr, cashboxA[1] = Byn, cashboxA[2] = USD
+    var cashboxA = []; // we store the result of calculateCashDelta in it
+
+    for(var cycleTimeDay = startTimeDay; cycleTimeDay <= finishTimeDay; cycleTimeDay++){
+        var cycleData = new Date();
+        cycleData.setTime(cycleTimeDay*1000*60*60*24);
+        cashboxA = calculateCashDelta(cycleTimeDay);
+        for(var i = 0; i<flowcashboxA.length; i++){// we increment flowcashboxA with values of cashboxA
+            flowcashboxA[i] = flowcashboxA[i] + cashboxA[i];
+        }// flowcashboxA has an actual amount of money on the cycleTimeDay
+        writeCashFlow(cycleTimeDay, flowcashboxA[0], flowcashboxA[1], flowcashboxA[2]);
+        ifWeNeedExchange(cycleTimeDay, flowcashboxA[0], flowcashboxA[1], flowcashboxA[2]); // I have no idea to use it
+    }
 }
 
 //runCashFlow(findStartData(ratesH), findFinishData(ratesH));//start CashFlow
+
+db.cashflow.remove({});
 
 runCashFlowPLus(findStartData(ratesH), findFinishData(ratesH)); //start CashFlowPlus
